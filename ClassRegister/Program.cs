11fa1b8e70cc2c679b7a41452cfdd2fe67d67faa
Jhay <61503572+Jhayokereke@users.noreply.github.com>
2022-02-04 14:25:30 +0100

    post class

---
+++ ClassRegister/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace ClassRegister
+{
+    class Program
+    {    
+
+        static void Main()
+        {
+            Student student = new Student
+            {
+                Firstname = "Uzor",
+                Lastname = "Nwachukwu",
+                Gender = 0,
+                Gadgets = new string[] { "Dell Laptop", "iPhone12 ProMax", "Backpack", "Face cap" }
+            };
+
+            Student student2 = new Student()
+            {
+                Firstname = "Isaac",
+                Lastname = "Raphael",
+                Gender = Gender.Male,
+                Gadgets = new string[] { "Hp Laptop", "Nokia 3310", "Backpack" }
+            };
+
+            Student[] students = new Student[]
+            {
+                student,
+                student2,
+                new Student()
+                {
+                    Firstname = "Sarah",
+                    Gender = Gender.Female,
+                    Gadgets = new string[] { "Hp Laptop", "Samsung galaxy s12", "Handbag" }
+                },
+                
+            };
+            DotNetClass netClass = new DotNetClass(students);
+            netClass.PrintClassDetails();
+        }
+    }
+}
+
+//class object
+//field representing total number of students
+//field representing the list of students in the class
+//method to set the number of students
+//method to add students to the class
+//method to remove student from class
+//class name
+//class description
+
+
+//student object
+//student name
+//student gender
+//student gadgets
+//student id
---
+++ ClassRegister/Student.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ClassRegister
+{
+    public class Student
+    {
+        public Student()
+        {
+            Id = Guid.NewGuid();
+        }
+        public Guid Id { get; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Fullname
+        {
+            get
+            {
+                return !string.IsNullOrWhiteSpace(Lastname) ? $"{Lastname} {Firstname}" : Firstname;
+            }
+        }
+        public Gender Gender { get; set; }
+        public string[] Gadgets { get; set; }
+
+        public override string ToString()
+        {
+            return $"Id: {Id}\nFullname: {Fullname}\nGender: {Gender}\nGadgets: {string.Join(", ", Gadgets)}\n";
+        }
+    }
+}
